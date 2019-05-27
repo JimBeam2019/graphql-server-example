2ea5a266a28bd7b9224b437bd26b48bbd2c15a76
JimBeam2019 <bingking68@hotmail.com>
2019-05-27 17:33:56 +0800

    Code organised.

---
+++ src/resolvers.js
@@ -0,0 +1,30 @@
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+    {
+        title: 'Harry Potter Series',
+        author: 'Ms J.K. Rowling',
+    },
+    {
+        title: 'Pet Sametary',
+        author: 'Mr Stephen King',
+    },
+    {
+        title: 'The Hunger Games',
+        author: 'S.C.'
+    },
+];
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+
+module.exports = {
+    resolvers
+};
